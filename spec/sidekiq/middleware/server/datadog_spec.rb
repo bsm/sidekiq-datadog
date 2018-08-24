7b4fd5a6d7b227b0b236d7e7bf0f7bd84c28540a
James Dabbs <jamesdabbs@gmail.com>
2018-08-24 11:34:28 -0700

    Flatten dynamic tags
    Right now we don't have a great way to write generic middleware that could generate 0-to-many tags. We can support this pretty easily by allowing tagging procs to return a list of tags, and flattening out the list before tagging.

--- spec/sidekiq/middleware/server/datadog_spec.rb
+++ spec/sidekiq/middleware/server/datadog_spec.rb
@@ -4,9 +4,12 @@ describe Sidekiq::Middleware::Server::Datadog do
 
   let(:statsd) { Mock::Statsd.new(nil, nil, {}, 10000) }
   let(:worker) { Mock::Worker.new }
+  let(:tags) {
+    ["custom:tag", lambda{|w, *| "worker:#{w.class.name[1..2]}" }]
+  }
 
   before  { statsd.written.clear }
-  subject { described_class.new hostname: "test.host", statsd: statsd, tags: ["custom:tag", lambda{|w, *| "worker:#{w.class.name[1..2]}" }] }
+  subject { described_class.new hostname: "test.host", statsd: statsd, tags: tags }
 
   it 'should send an increment and timing event for each job run' do
     subject.call(worker, { 'enqueued_at' => 1461881794.9312189 }, 'default') { "ok" }
@@ -35,6 +38,22 @@ describe Sidekiq::Middleware::Server::Datadog do
       "sidekiq.job:1|c|#custom:tag,worker:oc,host:test.host,env:test,name:mock/worker,status:error,error:runtime",
       "sidekiq.job.time:333|ms|#custom:tag,worker:oc,host:test.host,env:test,name:mock/worker,status:error,error:runtime",
     ])
+  end
+
+  context 'with a dynamic tag list' do
+    let(:tags) {
+      ["custom:tag", lambda {|w, j, *| j['args'].map { |n| "arg:#{n}"} }]
+    }
+
+    it 'should generate the correct tags' do
+      subject.call(worker, { 'enqueued_at' => 1461881794.9312189, 'args' => [1, 2] }, 'default') { "ok" }
+
+      expect(statsd.written).to eq([
+        "sidekiq.job:1|c|#custom:tag,arg:1,arg:2,host:test.host,env:test,name:mock/worker,queue:default,status:ok",
+        "sidekiq.job.time:333|ms|#custom:tag,arg:1,arg:2,host:test.host,env:test,name:mock/worker,queue:default,status:ok",
+        "sidekiq.job.queued_time:333|ms|#custom:tag,arg:1,arg:2,host:test.host,env:test,name:mock/worker,queue:default,status:ok"
+      ])
+    end
   end
 
 end
