879802fa86c72a3bfba02e2642dc52000c823736
Daniel Magliola <dmagliola@crystalgears.com>
2021-06-15 10:03:09 +0100

    Extract tag building logic into shareable class
    There is a good amount of logic in the server middleware to build the
    tags for the metrics we are reporting. This change extracts that logic
    into a separate class that we can reuse from other middleware.
    
    This is in preparation for adding metrics to the Client-side middleware
    (enqueuing jobs), which also needs most of this tags logic. (added in
    next commit)

--- lib/sidekiq/middleware/server/datadog.rb
+++ lib/sidekiq/middleware/server/datadog.rb
@@ -1,5 +1,6 @@
 require 'sidekiq'
 require 'sidekiq/datadog/version'
+require 'sidekiq/datadog/tag_builder'
 require 'datadog/statsd'
 require 'socket'
 
@@ -29,12 +30,11 @@ module Sidekiq
 
           @metric_name  = opts[:metric_name] || 'sidekiq.job'
           @statsd       = opts[:statsd] || ::Datadog::Statsd.new(statsd_host, statsd_port)
-          @tags         = opts[:tags] || []
-          @skip_tags    = Array(opts[:skip_tags]).map(&:to_s)
-
-          env  = Sidekiq.options[:environment] || ENV['RACK_ENV']
-          host = opts[:hostname] || ENV['INSTRUMENTATION_HOSTNAME'] || Socket.gethostname
-          setup_defaults(host: host, env: env)
+          @tag_builder  = Sidekiq::Datadog::TagBuilder.new(
+            opts[:tags],
+            opts[:skip_tags],
+            opts[:hostname],
+          )
         end
 
         def call(worker, job, queue, *)
@@ -54,7 +54,7 @@ module Sidekiq
 
         def record(worker, job, queue, start, clock, error = nil)
           msec = ::Process.clock_gettime(::Process::CLOCK_MONOTONIC, :millisecond) - clock
-          tags = build_tags(worker, job, queue, error)
+          tags = @tag_builder.build_tags(worker, job, queue, error)
 
           @statsd.increment @metric_name, tags: tags
           @statsd.timing "#{@metric_name}.time", msec, tags: tags
@@ -63,54 +63,6 @@ module Sidekiq
 
           queued_ms = ((start - Time.at(job['enqueued_at'])) * 1000).round
           @statsd.timing "#{@metric_name}.queued_time", queued_ms, tags: tags
-        end
-
-        def build_tags(worker, job, queue = nil, error = nil)
-          name = underscore(job['wrapped'] || worker.class.to_s)
-          tags = @tags.flat_map do |tag|
-            case tag
-            when String then tag
-            when Proc then tag.call(worker, job, queue, error)
-            end
-          end
-          tags.compact!
-
-          tags.push "name:#{name}" if include_tag?('name')
-          tags.push "queue:#{queue}" if queue && include_tag?('queue')
-
-          if error.nil?
-            tags.push 'status:ok' if include_tag?('status')
-          else
-            kind = underscore(error.class.name.sub(/Error$/, ''))
-            tags.push 'status:error' if include_tag?('status')
-            tags.push "error:#{kind}" if include_tag?('error')
-          end
-
-          tags
-        end
-
-        def setup_defaults(hash)
-          hash.each do |key, val|
-            key = key.to_s
-            next unless val && include_tag?(key)
-
-            prefix = "#{key}:"
-            next if @tags.any? {|t| t.is_a?(String) && t.start_with?(prefix) }
-
-            @tags.push [key, val].join(':')
-          end
-        end
-
-        def include_tag?(tag)
-          !@skip_tags.include?(tag)
-        end
-
-        def underscore(word)
-          word = word.to_s.gsub(/::/, '/')
-          word.gsub!(/([A-Z\d]+)([A-Z][a-z])/, '\1_\2')
-          word.gsub!(/([a-z\d])([A-Z])/, '\1_\2')
-          word.tr!('-', '_')
-          word.downcase
         end
       end
     end
